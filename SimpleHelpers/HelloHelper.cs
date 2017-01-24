7cb52e330f2e3af6a2f4609b9fa229246f40fae3
Philippe Matray <phmatray@outlook.com>
2017-01-24 19:07:39 +0100

    Add NuGet nuspec

---
+++ SimpleHelpers/HelloHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHelpers
+{
+    public static class HelloHelper
+    {
+        public static string Hello()
+            => "Hello SimpleHelper";
+
+        public static string Echo(this string s)
+            => s;
+    }
+}
